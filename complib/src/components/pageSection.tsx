24e1a92b9e5094ee80f9924c45c4f2c2b4284f23
Michael S <Michael.Stojanovic@hotmail.com>
2025-04-04 21:36:32 +0200

    minor css adjustment

--- complib/src/components/pageSection.tsx
+++ complib/src/components/pageSection.tsx
@@ -1,3 +1,5 @@
+import { CardBase } from './cards/cardbase';
+
 interface PageSectionProps {
   title: string;
   children: React.ReactNode;
@@ -7,11 +9,11 @@ export const PageSection = ({ title, children }: PageSectionProps) => {
   return (
     <section className='p-6 space-y-4 grid grid-cols-1 grid-rows-[20%_80%] h-full w-full '>
       <h1 className='text-4xl font-bold text-white text-center'>{title}</h1>
-      <div className='grid grid-cols-1 grid-rows-[30%_70%] gap-4 border border-amber-300 h-full'>
-        <div className='w-44 h-44 border justify-self-center self-center'>
-          Placeholder for component in focus
+      <div className='grid grid-cols-1 grid-rows-[40%_60%] border border-amber-300 h-full'>
+        <div className='w-full h-full border items-center justify-center flex'>
+          <CardBase children={undefined}></CardBase>
         </div>
-        <div className=' flex gap-6 justify-center py-4'>{children}</div>
+        <div className=' flex gap-6 justify-center py-4 border'>{children}</div>
       </div>
     </section>
   );
