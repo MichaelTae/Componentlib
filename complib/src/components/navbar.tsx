aecc5118d93802fe5d6597399e92728d0b342cca
Michael S <Michael.Stojanovic@hotmail.com>
2025-05-21 13:11:42 +0200

    added new func to navbar

--- complib/src/components/navbar.tsx
+++ complib/src/components/navbar.tsx
@@ -1,13 +1,19 @@
 import { navbarItems } from './navbarItems';
 import { navBarItemsData } from '../data/navbarItemData';
-const Navbar = () => {
+interface NavbarProps {
+  onNavItemClick: (item: string) => void;
+}
+const Navbar = ({ onNavItemClick }: NavbarProps) => {
+  const handleClick = (item: string) => {
+    onNavItemClick(item);
+  };
   return (
     <>
       <nav className='w-full flex justify-between shadow-md p-4  rounded-xl backdrop-blur-2xl bg-white/5 border border-neon-blue'>
         <div className='flex flex-col  w-full h-full'>
           <div>LOGO</div>
           <div>Navbar</div>
-          <div>{navbarItems(navBarItemsData)}</div>
+          <div>{navbarItems(navBarItemsData, handleClick)}</div>
         </div>
       </nav>
     </>
