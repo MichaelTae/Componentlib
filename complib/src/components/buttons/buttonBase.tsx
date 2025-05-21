cee60d371ef7278fca631aeea0b1e9efefea9e08
Michael S <Michael.Stojanovic@hotmail.com>
2025-05-21 12:10:04 +0200

    added button basecomponent

---
+++ complib/src/components/buttons/buttonBase.tsx
@@ -0,0 +1,42 @@
+interface ButtonBaseProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'danger' | 'tertiary';
+  size?: 'small' | 'medium' | 'large';
+  className?: string;
+  isLoading?: boolean;
+  icon?: React.ReactNode;
+}
+
+export const ButtonBase: React.FC<ButtonBaseProps> = ({
+  variant = 'primary',
+  size = 'medium',
+  className = '',
+  isLoading = false,
+  icon,
+  children,
+  ...rest
+}) => {
+  const variantStyles: Record<string, string> = {
+    primary: 'bg-cyan-400 text-black hover:bg-cyan-300',
+    secondary: 'bg-gray-500 text-white hover:bg-gray-700',
+    danger: 'bg-red-500 text-white hover:bg-red-600',
+    tertiary: 'bg-transparent text-blue-500 hover:bg-blue-100',
+  };
+
+  const sizeStyles: Record<string, string> = {
+    small: 'px-2 my-8 text-sm',
+    medium: 'px-4 my-7 text-base',
+    large: 'px-6 my-6 text-lg',
+  };
+
+  return (
+    <button
+      className={`cursor-pointer rounded flex items-center justify-center gap-2 bg- ${variantStyles[variant]} ${sizeStyles[size]} ${className}`}
+      disabled={isLoading}
+      {...rest}
+    >
+      {isLoading ? <span /> : icon}
+      {children}
+    </button>
+  );
+};
