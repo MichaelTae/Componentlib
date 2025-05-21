ec7c6860963a8566ccc9bb0f2e87c5268942da45
Michael S <Michael.Stojanovic@hotmail.com>
2025-05-21 14:40:39 +0200

    added distinction between done/not done components in nav

--- complib/src/components/navbarItems.tsx
+++ complib/src/components/navbarItems.tsx
@@ -17,17 +17,22 @@ export const navbarItems = (
     <div className='space-y-2'>
       {items.map((item) => {
         const isVisible = visibleItems[item.title] || false;
+        const isCreated = item.created;
+        const isDisabled = !isCreated;
+        const itemCreatedColor = isCreated
+          ? 'bg-sky-950'
+          : 'bg-sky-950/50 pointer-events-none';
         return (
           <div
             key={item.title}
-            className='border border-neon-blue rounded-lg shadow-sm bg-sky-950'
-            onClick={() => handleClick(item.title)}
+            className={`border border-neon-blue rounded-lg shadow-sm ${itemCreatedColor}`}
+            onClick={() => (isCreated ? handleClick(item.title) : null)}
           >
             <button
               onClick={() => toggleVisibility(item.title)}
               className=' px-4 py-2 w-full text-left font-semibold text-gray-300 hover:text-gray-100 cursor-pointer  '
             >
-              {item.title}
+              {isCreated ? item.title : `${item.title} -- (Not created yet)`}
             </button>
             {isVisible && (
               <div className='p-4 my-2 ml-2 space-y-1.5 shadow-md border-l border-gray-600/50 rounded-lg'>
