e22f854ce94debba904a13c49327df115f658cde
Michael S <Michael.Stojanovic@hotmail.com>
2025-05-21 14:43:41 +0200

    added forms to componentgroups func

--- complib/src/utils/hooks/useComponentGroup.ts
+++ complib/src/utils/hooks/useComponentGroup.ts
@@ -1,12 +1,14 @@
 import { useCards } from './useCards';
 import { useButtons } from './useButtons';
+import { useForms } from './useForms';
 
 export function useComponentGroups() {
   const cards = useCards();
   const buttons = useButtons();
-
+  const forms = useForms();
   return {
     Cards: cards,
     Buttons: buttons,
+    Forms: forms,
   };
 }
