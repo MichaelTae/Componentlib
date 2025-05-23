ce92cdc50c7ed3cd66ac14439b38eccab2b890c4
Michael S <Michael.Stojanovic@hotmail.com>
2025-05-23 15:12:27 +0200

    added new comps to hook

--- complib/src/utils/hooks/useComponentGroup.ts
+++ complib/src/utils/hooks/useComponentGroup.ts
@@ -1,14 +1,20 @@
 import { useCards } from './useCards';
 import { useButtons } from './useButtons';
 import { useForms } from './useForms';
+import { useModals } from './useModals';
+import { useTables } from './useTables';
 
 export function useComponentGroups() {
   const cards = useCards();
   const buttons = useButtons();
   const forms = useForms();
+  const modals = useModals();
+  const tables = useTables();
   return {
     Cards: cards,
     Buttons: buttons,
     Forms: forms,
+    Modals: modals,
+    Tables: tables,
   };
 }
